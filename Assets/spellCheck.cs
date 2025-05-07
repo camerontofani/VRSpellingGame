c75147089c1603b00168713453b7f7c91289fe1b
malston0 <98547501+malston0@users.noreply.github.com>
2025-05-07 17:12:36 -0500

    with correct/incorrect in scene

--- Assets/spellCheck.cs
+++ Assets/spellCheck.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class SpellChecker : MonoBehaviour
 {
     public SnapZoneTracker[] snapZones; // Assign in Inspector
     public string targetWord = "ACHIEVE"; // Editable per object
+    public TextMeshProUGUI feedbackText;
 
     public void CheckSpelling()
     {
@@ -23,13 +25,37 @@ public class SpellChecker : MonoBehaviour
         if (userWord == targetWord.ToUpper())
         {
             Debug.Log("Correct! You spelled " + targetWord + ".");
+            //feedbackText.text = "Correct!";
+            ShowFeedback("Correct!", Color.green);
         }
         else
         {
             Debug.Log("Incorrect. Try again.");
+            // feedbackText.text = "Try again!";
+            ShowFeedback("Try again!", Color.red);
         }
     }
+
+    private void ShowFeedback(string message, Color color)
+    {
+        if (feedbackText != null)
+        {
+            feedbackText.gameObject.SetActive(true);
+            feedbackText.text = message;
+            feedbackText.color = color;
+            //feedbackText.gameObject.SetActive(true);
+            StopAllCoroutines(); // Cancel any previous hides
+            StartCoroutine(HideFeedbackAfterDelay(3f));
+        }
+    }
+
+    private IEnumerator HideFeedbackAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        feedbackText.gameObject.SetActive(false);
+    }
 }
+
 
 
 
